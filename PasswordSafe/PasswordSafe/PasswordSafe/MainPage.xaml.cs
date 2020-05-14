2478bc61b1452e0b8bcd95d095b7c7a1f69c2034
Simon Lacroix <simlacroix@yahoo.ca>
2020-05-14 19:23:55 -0400

    Recreate Project

--- PasswordSafe/PasswordSafe/PasswordSafe/MainPage.xaml.cs
+++ PasswordSafe/PasswordSafe/PasswordSafe/MainPage.xaml.cs
@@ -9,7 +9,7 @@ using Xamarin.Forms;
 namespace PasswordSafe
 {
     // Learn more about making custom code visible in the Xamarin.Forms previewer
-    // by visiting https://aka.ms/xamarinformspublicpreviewer
+    // by visiting https://aka.ms/xamarinforms-previewer
     [DesignTimeVisible(false)]
     public partial class MainPage : ContentPage
     {
