4b9bba8218d98e9585b0018b32f45e4b4bda0129
Simon Lacroix <simlacroix@yahoo.ca>
2020-05-15 12:41:15 -0400

    Design of start page complete for now

--- PasswordSafe/PasswordSafe/PasswordSafe/StartPage.xaml.cs
+++ PasswordSafe/PasswordSafe/PasswordSafe/StartPage.xaml.cs
@@ -16,5 +16,10 @@ namespace PasswordSafe
         {
             InitializeComponent();
         }
+
+        private void access_Clicked(object sender, EventArgs e)
+        {
+
+        }
     }
 }
