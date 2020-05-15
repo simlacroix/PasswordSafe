673cb8474bdffd445bb0d4efa9a6b4933ffdd324
Simon Lacroix <simlacroix@yahoo.ca>
2020-05-15 17:15:07 -0400

    Start Page functionality

--- PasswordSafe/PasswordSafe/PasswordSafe/StartPage.xaml.cs
+++ PasswordSafe/PasswordSafe/PasswordSafe/StartPage.xaml.cs
@@ -18,13 +18,22 @@ namespace PasswordSafe
         }
 
         private void access_Clicked(object sender, EventArgs e)
-        {
-
+        {            
+            if (!App.PassOperations.CheckPass())
+            {
+                DisplayAlert("Error", "No combination has been defined yet, click the button below to set your Password Safe combination", "ok");
+                return;
+            }
+            string safeCombination = combination.Text;
+            if (App.PassOperations.VerifyPass(safeCombination))
+            {
+                //navigate to password list page
+            }
         }
 
         private void change_Clicked(object sender, EventArgs e)
         {
-
+            Navigation.PushAsync(new CombinationPage());
         }
     }
 }
