51d8681fb8e819dace1963e144284044685e7555
Simon Lacroix <simlacroix@yahoo.ca>
2020-05-15 17:43:05 -0400

    Combination Page background code

--- PasswordSafe/PasswordSafe/PasswordSafe/CombinationPage.xaml.cs
+++ PasswordSafe/PasswordSafe/PasswordSafe/CombinationPage.xaml.cs
@@ -12,14 +12,43 @@ namespace PasswordSafe
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class CombinationPage : ContentPage
     {
+        private bool oldRemoved = false;
+
         public CombinationPage()
         {
             InitializeComponent();
+            if (!App.PassOperations.CheckPass())
+            {
+                Table.Root.Remove(oldSection);
+                oldRemoved = true;
+            }
+            else
+            {
+                oldRemoved = false;
+            }
+                
+            
         }
 
-        private void submit_Clicked(object sender, EventArgs e)
+        private async void submit_Clicked(object sender, EventArgs e)
         {
-
+            if (!oldRemoved)
+            {
+                if (!App.PassOperations.VerifyPass(old.Text))
+                {
+                    await DisplayAlert("Error", "The old combination was incoreclty entered, please try again", "ok");
+                    return;
+                }                 
+            }
+            
+            if (theNew.Text == confirm.Text)
+            {
+                await DisplayAlert("Confirmed", "Combination was successfully changed", "ok");
+                App.PassOperations.ChangePass(theNew.Text);
+                await Navigation.PopAsync();
+            }
+            else
+                await DisplayAlert("Error", "Combination must be the same in both fields", "ok");
         }
     }
 }
