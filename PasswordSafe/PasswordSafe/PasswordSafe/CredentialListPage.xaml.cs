3f02b533c78aec2277e97a349eeade3bd59ab4cd
xinyuema0126 <1142613269@qq.com>
2020-05-16 15:23:56 -0400

    Created CredentialListPage

---
+++ PasswordSafe/PasswordSafe/PasswordSafe/CredentialListPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using PasswordSafe.Models;
+using System.Collections.ObjectModel;
+
+namespace PasswordSafe
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class CredentialListPage : ContentPage
+    {
+        private ObservableCollection<Credential> _credentials;
+        private string sortKeyword;
+        public CredentialListPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
